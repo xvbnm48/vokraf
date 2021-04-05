14abefb57418b8a5fa180a65e9dadb8a3bebe1f8
xvbnm48 <muhamadfarizwisnuprananda@gmail.com>
2021-04-05 23:27:25 +0700

    voucher

---
+++ voucher.js
@@ -0,0 +1,12 @@
+function voucher(length) {
+  var result = "";
+  const kode = ("TGD", "YCC", "SKU");
+  var characters = "1234567890QWERTYUIOPASDFGHJKLZXCVBNM";
+  var charactersLength = characters.length;
+  for (var i = 0; i < 12; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return kode + result;
+}
+
+console.log(voucher(5));
