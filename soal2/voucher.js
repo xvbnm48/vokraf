b1e1b209306c1793213ad0843aac7903f982d7f4
xvbnm48 <muhamadfarizwisnuprananda@gmail.com>
2021-04-06 08:24:27 +0700

    soal2

--- soal2/voucher.js
+++ soal2/voucher.js
@@ -1,12 +1,13 @@
 function voucher(length) {
   var result = "";
-  const kode = ("TGD", "YCC", "SKU");
+  const arr = ("TGD", "YCC", "SKU");
+  arr[Math.floor(Math.random() * arr.length)];
   var characters = "1234567890QWERTYUIOPASDFGHJKLZXCVBNM";
   var charactersLength = characters.length;
   for (var i = 0; i < 12; i++) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
-  return kode + result;
+  return arr + result;
 }
 
 console.log(voucher(2));
